0b9c3dc782e6af8bc9a90a7d4c0a1e2e8e9ee249
Peter Oberndorfer <kumbayo84@arcor.de>
2025-11-08 22:36:59 +0100

    Remove MeanBean tests and dependency on meanbean
    Reduce the number of dependencies.

--- src/test/java/com/paypal/heapdumptool/capture/CaptureCommandTest.java
+++ src/test/java/com/paypal/heapdumptool/capture/CaptureCommandTest.java
@@ -1,20 +1,10 @@
 package com.paypal.heapdumptool.capture;
 
-import com.paypal.heapdumptool.sanitizer.DataSize;
 import org.junit.jupiter.api.Test;
-import org.meanbean.test.BeanVerifier;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CaptureCommandTest {
-
-    @Test
-    public void testBean() {
-        BeanVerifier.forClass(CaptureCommand.class)
-                    .withSettings(settings -> settings.addOverridePropertyFactory(CaptureCommand::getBufferSize, () -> DataSize.ofMegabytes(5)))
-                    .withSettings(settings -> settings.addIgnoredPropertyName("excludeStringFields"))
-                    .verifyGettersAndSetters();
-    }
 
     @Test
     public void testSanitizationText() {
--- src/test/java/com/paypal/heapdumptool/sanitizer/SanitizeCommandTest.java
+++ src/test/java/com/paypal/heapdumptool/sanitizer/SanitizeCommandTest.java
@@ -1,20 +1,10 @@
 package com.paypal.heapdumptool.sanitizer;
 
 import org.junit.jupiter.api.Test;
-import org.meanbean.test.BeanVerifier;
 
-import static com.paypal.heapdumptool.sanitizer.DataSize.ofMegabytes;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SanitizeCommandTest {
-
-    @Test
-    public void testBean() {
-        BeanVerifier.forClass(SanitizeCommand.class)
-                    .withSettings(settings -> settings.addOverridePropertyFactory(SanitizeCommand::getBufferSize, () -> ofMegabytes(5)))
-                    .withSettings(settings -> settings.addIgnoredPropertyName("excludeStringFields"))
-                    .verifyGettersAndSetters();
-    }
 
     @Test
     public void testSanitizationText() {
